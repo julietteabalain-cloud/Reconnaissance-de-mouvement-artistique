a745b9e03b857459db25e8961f355a6429862c83
Juliette Abalain <juliette.abalain@student-cs.fr>
2026-01-13 15:12:21 +0100

    Code de téléchargement du Dataset sur Google Drive

--- src/dataset.py
+++ src/dataset.py
@@ -1,81 +1,16 @@
-from datasets import load_dataset
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-from collections import defaultdict
-import random
+from matplotlib import pyplot as plt
 
-#1. CHARGER LE DATASET`
+################### EXPLORATION DES DONNEES ###################
 
-dataset = load_dataset("huggan/wikiart", split="train")
-
-#2. LIMITER À X IMAGES PAR STYLE`
-
-def limit_number_images(dataset, max_images_per_style=400):
-
-    style_indices = defaultdict(list)
-    for idx, item in enumerate(dataset):
-        style_indices[item['style']].append(idx)
-
-    selected = []
-    for style, indices in style_indices.items():
-        selected.extend(random.sample(indices, min(len(indices), max_images_per_style)))
-
-    balanced_dataset = dataset.select(selected)
-    return selected,balanced_dataset
-
-selected, balanced_dataset = limit_number_images(dataset)
-
-#3. SPLIT 70/15/15
-
-def split(selected):
-    random.shuffle(selected)
-    n = len(selected)
-    train_idx = selected[:int(0.7*n)]
-    val_idx = selected[int(0.7*n):int(0.85*n)]
-    test_idx = selected[int(0.85*n):]
-    return train_idx,val_idx,test_idx
-
-train_idx, val_idx, test_idx = split(selected)
-
-#4. PYTORCH DATASET
-
-class WikiArtDataset(Dataset):
-    def init(self, dataset, indices, transform):
-        self.dataset = dataset
-        self.indices = indices
-        self.transform = transform
-        styles = sorted(set(dataset[i]['style'] for i in indices))
-        self.style_to_label = {s: i for i, s in enumerate(styles)}
-
-    def __len__(self):
-        return len(self.indices)
-
-    def __getitem__(self, idx):
-        item = self.dataset[self.indices[idx]]
-        image = item['image'].convert('RGB')
-        image = self.transform(image)
-        label = self.style_to_label[item['style']]
-        return image, label
-
-#5. DATALOADERS
-
-def dataloader(balanced_dataset, train_idx, val_idx, test_idx, WikiArtDataset):
-    transform = transforms.Compose([
-                        transforms.Resize((224, 224)),
-                        transforms.ToTensor(),
-                        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-                        ])
-
-    train_loader = DataLoader(WikiArtDataset(balanced_dataset, train_idx, transform), batch_size=32, shuffle=True)
-    val_loader = DataLoader(WikiArtDataset(balanced_dataset, val_idx, transform), batch_size=32)
-    test_loader = DataLoader(WikiArtDataset(balanced_dataset, test_idx, transform), batch_size=32)
-    print(f"✅ Prêt ! {len(train_idx)} train, {len(val_idx)} val, {len(test_idx)} test")
-    return train_loader, val_loader, test_loader
-
-train_loader, val_loader, test_loader = dataloader(balanced_dataset, train_idx, val_idx, test_idx, WikiArtDataset)
-
-def load_and_select_dataloaders():
-    dataset = load_dataset("huggan/wikiart", split="train")
-    selected, balanced_dataset = limit_number_images(dataset)
-    train_idx, val_idx, test_idx = split(selected)
-    return dataloader(balanced_dataset, train_idx, val_idx, test_idx, WikiArtDataset)
+def show_images_by_style(dataset, target_style, n=5):
+    count = 0
+    for item in dataset:
+        if item["style"] == target_style:
+            plt.figure(figsize=(3,3))
+            plt.imshow(item["image"])
+            plt.axis("off")
+            plt.title(f"{item['artist']} – {item['style']}")
+            plt.show()
+            count += 1
+            if count == n:
+                break
---
+++ src/load_dataset.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+import csv
+from tqdm import tqdm
+import random
+from sklearn.model_selection import train_test_split
+import random
+from collections import defaultdict
+from datasets import load_dataset
+from google.colab import drive
+
+########### enregistrement d'un sous-ensemble du dataset WikiArt sur google Drive ###########
+
+drive.mount('/content/drive')
+
+dataset = load_dataset(
+    "huggan/wikiart",
+    split="train",
+)
+
+MAX_PER_STYLE = 400
+
+# Compteur par style
+counter = defaultdict(int)
+
+def select_fn(example):
+    style = example["style"]
+    if counter[style] < MAX_PER_STYLE:
+        counter[style] += 1
+        return True
+    return False
+
+subset = dataset.shuffle(seed=42).filter(select_fn)
+
+# Récupère les indices du dataset filtré
+indices = list(range(len(subset)))
+
+# Shuffle pour mélanger
+random.seed(42)
+random.shuffle(indices)
+
+# Split 70/15/15
+train_idx, test_idx = train_test_split(indices, test_size=0.3, random_state=42)
+val_idx, test_idx = train_test_split(test_idx, test_size=0.5, random_state=42)
+
+print(f"Train: {len(train_idx)}, Val: {len(val_idx)}, Test: {len(test_idx)}")
+
+BASE_DIR = Path("/content/drive/MyDrive/DeepLearning/WikiArt_Subset")
+BASE_DIR.mkdir(parents=True, exist_ok=True)
+
+splits = {
+    "train": train_idx,
+    "val": val_idx,
+    "test": test_idx
+}
+
+for split_name, split_indices in splits.items():
+    csv_path = BASE_DIR / f"{split_name}_metadata.csv"
+
+    with open(csv_path, "w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(
+            f,
+            fieldnames=["filename", "style", "artist", "genre"]
+        )
+        writer.writeheader()
+
+        for idx in tqdm(split_indices, desc=f"Saving {split_name}"):
+            item = subset[idx]
+
+            style = str(item["style"])
+            artist = item["artist"]
+            genre = item["genre"]
+            image = item["image"].convert("RGB")
+
+            out_dir = BASE_DIR / split_name / style
+            out_dir.mkdir(parents=True, exist_ok=True)
+
+            filename = f"{idx}.jpg"
+            image_path = out_dir / filename
+
+            image.save(
+                image_path,
+                format="JPEG",
+                quality=90,
+                subsampling=2,
+                optimize=True
+            )
+
+            writer.writerow({
+                "filename": filename,
+                "style": style,
+                "artist": artist,
+                "genre": genre
+            })
+
