bafba225aa17f448d3db567974859a0e56aa8914
Juliette Abalain <juliette.abalain@student-cs.fr>
2026-02-19 13:10:11 +0100

    Test evaluation

--- src/dataset_dl.py
+++ src/dataset_dl.py
@@ -21,7 +21,7 @@ class ArtDataset(Dataset):
 
         image = Image.open(img_path).convert("RGB")
 
-        label = row["style_encoded"]  
+        label = row["style"]  
         
         if self.transform:
             image = self.transform(image)
