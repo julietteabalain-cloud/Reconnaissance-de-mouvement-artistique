ef0d8f9643f0f4dd86be36e16b3737d2731ea035
Juliette Abalain <juliette.abalain@student-cs.fr>
2026-02-23 20:19:34 +0100

    Architectures

--- src/models.py
+++ src/models.py
@@ -39,3 +39,56 @@ def get_resnet18(num_classes, device, freeze_backbone=True, drop=False):
             param.requires_grad = True
 
     return model
+
+
+def get_model(model_name, num_classes=23, dropout_p=0.2, freeze_backbone=True):
+
+    if model_name == "resnet34":
+        model = models.resnet34(weights=models.ResNet34_Weights.IMAGENET1K_V1)
+        in_features = model.fc.in_features
+        model.fc = nn.Sequential(
+            nn.Dropout(p=dropout_p),
+            nn.Linear(in_features, num_classes)
+        )
+
+    elif model_name == "mobilenet_v2":
+        model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V1)
+        in_features = model.classifier[1].in_features
+        model.classifier = nn.Sequential(
+            nn.Dropout(p=dropout_p),
+            nn.Linear(in_features, num_classes)
+        )
+
+    elif model_name == "efficientnet_b0":
+        model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.IMAGENET1K_V1)
+        in_features = model.classifier[1].in_features
+        model.classifier = nn.Sequential(
+            nn.Dropout(p=dropout_p),
+            nn.Linear(in_features, num_classes)
+        )
+
+    elif model_name == "efficientnet_b1":
+        model = models.efficientnet_b1(weights=models.EfficientNet_B1_Weights.IMAGENET1K_V1)
+        in_features = model.classifier[1].in_features
+        model.classifier = nn.Sequential(
+            nn.Dropout(p=dropout_p),
+            nn.Linear(in_features, num_classes)
+        )
+
+    else:
+        raise ValueError("Model name not recognized")
+
+    # Freeze backbone if requested
+    if freeze_backbone:
+        for param in model.parameters():
+            param.requires_grad = False
+        
+        # Unfreeze classifier layer
+        if "resnet" in model_name:
+            for param in model.fc.parameters():
+                param.requires_grad = True
+        else:
+            for param in model.classifier.parameters():
+                param.requires_grad = True
+
+    return model
